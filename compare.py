af31ab8f745b8dea4139bf103ca959c358554d54
Raveendra <sooriravindra@gmail.com>
2014-05-25 17:29:16 +0530

    Added comments

--- compare.py
+++ compare.py
@@ -17,7 +17,7 @@ print "Black aces are ", BLACK_ACE1 , BLACK_ACE2
 
 
 
-prob = [0]*52
+count = [0]*52 #Used to store count for each position
 
 for x in range(NUM_OF_SIM):
 	pack=[0]*52
@@ -30,9 +30,9 @@ for x in range(NUM_OF_SIM):
 		pack[i] = card
 		i+=1
 
-	pos = max(pack.index(BLACK_ACE1),pack.index(BLACK_ACE2))
-	prob[pos] = prob[pos]+1
-	if(x%STEPS == 0):
+	pos = max(pack.index(BLACK_ACE1),pack.index(BLACK_ACE2)) # Choose index of 2nd black ace
+	count[pos] = count[pos]+1
+	if(x%STEPS == 0):			# For output purpose
 		if(x%(STEPS*4)==0):
 			print "\b\b\b\b\b\b\b\b\b---",
 		elif(x%(STEPS*4)==STEPS):
@@ -43,9 +43,6 @@ for x in range(NUM_OF_SIM):
 			print "\b\b\b\b\b\b\b\b\b / ",
 		print "%3.0f"%(100*x/NUM_OF_SIM)+"%",
 		sys.stdout.flush()
-sum =0
-for each in prob:
-	sum+=each
 
 final_prob=[0]*52
 
@@ -54,10 +51,10 @@ print "Successfully simulated ",x+1,"cases.\nThe probability according to theore
 print "\tPos\tSimulated\tTheoretic\tDifference"
 
 for each in range(52):
-	final_prob[each]=1.0*prob[each]/sum
-	print "\t",each,"\t"," %.5f"%final_prob[each],
-	theoretic = 2.0*each/52/51
-	print "\t","%.5f"%theoretic,"\t %.5f"%abs(theoretic-final_prob[each])
+	final_prob[each]=1.0*count[each]/NUM_OF_SIM
+	print "\t",each,"\t"," %.5f"%final_prob[each], # Formatted output - pos,simulated probability
+	theoretic = 2.0*each/52/51     #Theoretically at pos x probability is 2*x/(52*51)
+	print "\t","%.5f"%theoretic,"\t %.5f"%abs(theoretic-final_prob[each]) # Formatted output theoretic probability
 	
 
 #print final_prob
